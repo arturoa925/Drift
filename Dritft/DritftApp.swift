05d2798b845f5de5bcc7d9b746a77bb48a4e846d
arturoa925 <ayalaarturo925@gmail.com>
2026-05-28 18:17:12 -0700

    Initial Commit

---
+++ Dritft/DritftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DritftApp.swift
+//  Dritft
+//
+//  Created by Arturo Ayala on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct DritftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DritftTests/DritftTests.swift
@@ -0,0 +1,19 @@
+//
+//  DritftTests.swift
+//  DritftTests
+//
+//  Created by Arturo Ayala on 5/28/26.
+//
+
+import Testing
+@testable import Dritft
+
+struct DritftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
